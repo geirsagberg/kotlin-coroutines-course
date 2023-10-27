b047bf9e667af3c543243f1c7d81f7cce2aec780
Geir Sagberg <geir.sagberg@gmail.com>
2023-10-27 10:57:25 +0200

    Improve morning routine

--- src/main/java/net/sagberg/pure/MorningRoutine.kt
+++ src/main/java/net/sagberg/pure/MorningRoutine.kt
@@ -1,34 +1,42 @@
 package net.sagberg.pure
 
 import java.util.*
-import kotlin.coroutines.*
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 suspend fun main() {
     morningRoutine()
 }
 
 suspend fun morningRoutine() {
-    val context = EmptyCoroutineContext
-
-    val coffeeBrewing = CompletableDeferred<Coffee>()
-    val catPeeing = CompletableDeferred<Unit>()
-
-    // 1. Inline declaration
-    suspend {
-        println("Brewing coffee...")
-        delay(3000) {
-            println("Coffee is ready!")
-            coffeeBrewing.complete(Coffee)
-        }
-    }.startCoroutine(Continuation(context) {})
-    // 2. Creation of a suspend function
-    catPeeingCoroutine(catPeeing)
-        .startCoroutine(Continuation(context) {})
+    val coffeeBrewing = brewCoffee()
+    val catPeeing = letOutCat()
 
     brushTeeth()
     catPeeing.await()
     val coffee = coffeeBrewing.await()
     startWorkingDay(coffee)
+}
+
+fun brewCoffee(): CompletableDeferred<Coffee> {
+    val coffeeBrewing = CompletableDeferred<Coffee>()
+    println("Brewing coffee...")
+    delay(3000) {
+        println("Coffee is ready!")
+        coffeeBrewing.complete(Coffee)
+    }
+    return coffeeBrewing
+}
+
+fun letOutCat(): CompletableDeferred<Unit> {
+    val catPeeing = CompletableDeferred<Unit>()
+    println("Letting the cat out...")
+    delay((Math.random() * 6000).toLong()) {
+        println("Meow! *scratch* *scratch*")
+        catPeeing.complete(Unit)
+    }
+    return catPeeing
 }
 
 class CompletableDeferred<T> {
@@ -47,28 +55,22 @@ class CompletableDeferred<T> {
     }
 }
 
-fun catPeeingCoroutine(result: CompletableDeferred<Unit>): suspend () -> Unit = {
-    println("Letting the cat out...")
-    delay((Math.random() * 6000).toLong()) {
-        println("Meow! *scratch* *scratch*")
-        result.complete(Unit)
-    }
-}
-
 val delayTimer = Timer()
 
 fun delay(timeMillis: Long, action: () -> Unit) {
     delayTimer.schedule(object : TimerTask() {
         override fun run() {
             action()
-            delayTimer.cancel()
         }
     }, timeMillis)
 }
 
 data object Coffee
 
-fun brushTeeth() {}
+fun brushTeeth() {
+    println("Brushing teeth...")
+}
+
 fun startWorkingDay(coffee: Coffee) {
     println("Starting work with $coffee")
 }
