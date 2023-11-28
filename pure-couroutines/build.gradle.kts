30d9a7cb19fec75932ebf4a4bdddeb86f236680c
Geir Sagberg <geir.sagberg@gmail.com>
2023-11-28 13:53:11 +0100

    Add pure-coroutines gradle project

---
+++ pure-couroutines/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "net.sagberg"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
---
+++ pure-couroutines/src/main/kotlin/MorningRoutine.kt
@@ -0,0 +1,9 @@
+package net.sagberg
+
+suspend fun main() {
+    // Brew coffee
+    // Let out cat
+    // Brush teeth
+    // Wait for coffee and cat
+    // Start working day
+}
